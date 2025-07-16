f5d19965d156b2bbf9dcac0501f78b99ce519aa8
hittaLautaro <hitta.lautaro@gmail.com>
2025-07-16 15:35:49 -0300

    improved security and added lazy fetch on some entities

--- src/main/java/com/hitta/ContractApp/service/CustomUserDetailsService.java
+++ src/main/java/com/hitta/ContractApp/service/CustomUserDetailsService.java
@@ -1,34 +1,24 @@
 package com.hitta.ContractApp.service;
 
-import com.hitta.ContractApp.model.CustomUserDetails;
 import com.hitta.ContractApp.model.Users;
 import com.hitta.ContractApp.repo.UserRepo;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserDetailsServiceImpl implements UserDetailsService {
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
 
     private final UserRepo userRepo;
 
-    public UserDetailsServiceImpl(UserRepo userRepo) {
-        this.userRepo = userRepo;
-    }
-
-
-
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-
-        Users user = userRepo.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("Username not found."));
-        if(user == null){
-            System.out.println("User "+ email +" not found");
-            throw new UsernameNotFoundException("User not found");
-        }
+        Users user = userRepo.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
 
         return new CustomUserDetails(user);
     }
-
 }
